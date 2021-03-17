b3e12ef86b04166270bd94af2182120f1e9f1010
wassilva <wash.bsilva@gmail.com>
2021-03-17 01:38:19 +0000

    test 17032021

--- index.js
+++ index.js
@@ -2,6 +2,16 @@
     const axios = require('axios');
 
     http.createServer((req, res)=>{
-      res.write("Hello World WASHY \n"); // write a response
+      res.write(users.join("\n")); // write a response
       res.end(); //end the response
     }).listen(8000); // listen for requests on port 8000
+
+    let users = []; // names of users will be stored here
+(async function getNames(){
+  try{
+    const {data} = await axios.get("https://jsonplaceholder.typicode.com/users");
+    users = data.map(user=>user.name);
+  } catch(error){
+    console.log(error)
+  }
+})()
